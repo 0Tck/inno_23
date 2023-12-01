b2aa28fe980b3c76af1f663852496c5659d9e0ab
unknown <tallurinanitck@gmail.com>
2023-12-02 01:30:28 +0530

    multilingual

--- app.py
+++ app.py
@@ -121,7 +121,13 @@ def onvisit():
 def search():
     return redirect(url_for('onvisit'))
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
